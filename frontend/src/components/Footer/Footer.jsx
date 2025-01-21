e1995df6b119ca7f48c0ddd443abe8f8781b163c
unknown <vermanjali721@gmail.com>
2025-01-21 16:51:02 +0530

    Completed Frontend

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
     <div className='footer' id='footer'>
         <div className="footer-content">
             <div className="footer-content-left">
-                <img src={assets.logo} alt="" />
+                <img src={assets.logo_footer} alt="" />
                 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque, earum.</p>
                 <div className="footer-social-icons">
                     <img src={assets.facebook_icon} alt="" />
